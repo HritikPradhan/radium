b1e8761fcdf9e4fd9fea8df6592bec37f6eb2e8c
HritikPradhan <hritikpradhan10@gmail.com>
2021-11-08 23:22:54 +0530

    module-system assignment

---
+++ assignment/src/util/logger.js
@@ -0,0 +1,10 @@
+function log(name){
+    console.log(name)
+}
+function welcome(){
+    console.log('welcome to my application,I am FunctionUp')
+}
+const url='https://www.functionup.org/'
+module.exports.log=log;
+module.exports.welcome=welcome;
+module.exports.url=url;
---
+++ assignment/validator/formatter.js
@@ -0,0 +1,12 @@
+function trimString(){
+    console.log("   Hritik   ".trim())
+}
+function changetoLowerCase(){
+    console.log("HRITIK pradhan".toLowerCase())
+}
+function changetoUpperCase(){
+    console.log("hritik Pradhan".toUpperCase())
+}
+module.exports.trimString=trimString;
+module.exports.changetoLowerCase=changetoLowerCase;
+module.exports.changetoUpperCase=changetoUpperCase;
